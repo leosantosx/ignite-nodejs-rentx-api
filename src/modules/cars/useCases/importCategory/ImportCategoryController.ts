0b0cba78b523b00f89e343d32c8fe2c4fd71a012
Leonardo Santos <leonardosaint748@gmail.com>
2022-03-01 14:08:20 -0300

    refactor: add database to import category repository

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -2,18 +2,18 @@ import { Request, Response } from "express";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
-    constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
-    handle(request: Request, response: Response): Response {
-        const { file } = request
-        
-        if (!file) {
-            return response.status(400).send('File not sent') 
-        }
+  constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { file } = request;
 
-        this.importCategoryUseCase.execute(file)
-        
-        return response.status(201).send()
+    if (!file) {
+      return response.status(400).send("File not sent");
     }
+
+    const categories = await this.importCategoryUseCase.execute(file);
+
+    return response.status(201).json(categories);
+  }
 }
 
-export { ImportCategoryController }
+export { ImportCategoryController };
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,46 +1,50 @@
 import { CategoryRepository } from "../../repositories/implementations/CategoriesRepository";
-import fs from 'fs'
-import { parse as csvParse } from 'csv-parse'
+import fs from "fs";
+import { parse as csvParse } from "csv-parse";
 
 interface IImportCategory {
-    name: string
-    description: string
+  name: string;
+  description: string;
 }
 
 class ImportCategoryUseCase {
-    constructor(private categoryRepository: CategoryRepository){}
+  constructor(private categoryRepository: CategoryRepository) {}
 
-    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
-        return new Promise((resolve, reject) => {
-            const categories: IImportCategory[] = []
-            const stream = fs.createReadStream(file.path)
-            
-            const parseFile = csvParse()
-            
-            stream.pipe(parseFile)
-            
-            parseFile.on('data', async (line) => {
-                const [name, description] = line
-                categories.push({
-                    name,
-                    description,
-                })
-            }).on("end", () => {
-                fs.promises.unlink(file.path)
-                resolve(categories)
-            }).on("error", (err) => {
-                reject(err)
-            })
-        })
-    }
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = [];
+      const stream = fs.createReadStream(file.path);
 
-    async execute(file: Express.Multer.File): Promise<void>{
-        const categories = await this.loadCategories(file)
-        categories.forEach(async category => {
-            const { name, description } = category
-            this.categoryRepository.create({ name, description })
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
         })
-    }
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<IImportCategory[]> {
+    const categories = await this.loadCategories(file);
+    categories.forEach(async (category) => {
+      const { name, description } = category;
+      this.categoryRepository.create({ name, description });
+    });
+    return categories;
+  }
 }
 
-export { ImportCategoryUseCase }
+export { ImportCategoryUseCase };
